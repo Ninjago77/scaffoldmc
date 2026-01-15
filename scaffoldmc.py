72662c8ad80a38b9ffcd6b73769eb7c0d08bc86b
Ninjago77 <shanvantharunmozhi@gmail.com>
2026-01-14 21:25:45 -0800

    settings readable and fixed newlines

--- scaffoldmc.py
+++ scaffoldmc.py
@@ -1,19 +1,24 @@
 SOURCE_DIR_URL = "https://raw.githubusercontent.com/Ninjago77/scaffoldmc/refs/heads/main/" # + "/scaffoldmc.py", etc....
-JSON_SETTINGS_PATH = ""
+JSON_SETTINGS_FILE_PATH = "scaffoldmc_settings.json"
 ######################## DO NOT EDIT AFTER THIS LINE ########################
 from urllib.request import urlopen, Request
+import json
 
-httprequest = Request(SOURCE_DIR_URL.strip("/") + "/scaffoldmc.py", headers={"Accept": "application/json"})
+with open(JSON_SETTINGS_FILE_PATH,"r") as settings_file:
+    SETTINGS = json.load(settings_file)
 
-with urlopen(httprequest) as response:
-    if not response.status == 200:
-        raise ConnectionRefusedError("Response Status is not '200' (OK), it is "+response.status)
-    with open(__file__,"r+") as file:
-        line1 = file.readline()
-        line2 = file.readline()
-    with open(__file__,"w+") as file:
-        file.write(
-            line1 + line2 + "\n" +
-            response.read().decode().split("\n",2)[2]            
-        )
+if SETTINGS["self-update"]:
+    httprequest = Request(SOURCE_DIR_URL.strip("/") + "/scaffoldmc.py", headers={"Accept": "application/json"})
+
+    with urlopen(httprequest) as response:
+        if not response.status == 200:
+            raise ConnectionRefusedError("Response Status is not '200' (OK), it is "+response.status)
+        with open(__file__,"r+") as file:
+            line1 = file.readline()
+            line2 = file.readline()
+        with open(__file__,"w+") as file:
+            file.write(
+                line1 + line2 +
+                response.read().decode().split("\n",2)[2]            
+            )
         
